35514a7995f8b94a71580035217934cf9a638e58
Mike <id69702509@yahoo.com.hk>
2017-06-07 22:52:07 +0800

    update

--- 32_cards/32.c
+++ 32_cards/32.c
@@ -4,16 +4,17 @@
 struct content
 {
     char name[20];
-    char value[15];
-    
+    char value[15][2];
 };
 
 int main()
 {
+    char value[13][2] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
     struct content club;
     struct content diamonds;
     struct content hearts;
     struct content spades;
+
     strcpy(club.name, "club");
     strcpy(diamonds.name, "diamonds");
     strcpy(hearts.name, "hearts");
@@ -21,40 +22,18 @@ int main()
 
     for (int i = 0; i < 13; i++)
     {
-        club.value[i]=i+'0';
-        diamonds.value[i]=i+'0';
-        hearts.value[i]=i+'0';
-        spades.value[i]=i+'0';
+        strcpy(club.value[i], value[i]);
+        strcpy(diamonds.value[i], value[i]);
+        strcpy(hearts.value[i], value[i]);
+        strcpy(spades.value[i], value[i]);
     }
-    club.value[0] = 'A';
-    diamonds.value[0] = 'A';
-    hearts.value[0] = 'A';
-    spades.value[0] = 'A';
 
-    
-	club.value[11] = 'j';
-    diamonds.value[11] = 'j';
-    hearts.value[11] = 'j';
-    spades.value[11] = 'j';
-
-     
-    club.value[12] = 'Q';
-    diamonds.value[12] = 'Q';
-    hearts.value[12] = 'Q';
-    spades.value[12] = 'Q';
-
-    club.value[13] = 'K';
-    diamonds.value[13] = 'K';
-    hearts.value[13] = 'K';
-    spades.value[13] = 'K';
-
-
-    for (int i = 0; i < 14; i++)
+    for (int i = 0; i < 13; i++)
     {
-        printf("club\t %c \t", club.value[i]);
-        printf("diamonds\t %c \t", diamonds.value[i]);
-        printf("hearts\t %c \t", hearts.value[i]);
-        printf("spades\t %c\n", spades.value[i]);
+        printf("club\t %s \t", club.value[i]);
+        printf("diamonds\t %s \t", diamonds.value[i]);
+        printf("hearts\t %s \t", hearts.value[i]);
+        printf("spades\t %s \t \n", spades.value[i]);
     }
 
     return 0;
