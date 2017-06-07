ca00de2de7ce4bf2fc34c91321d456362205bd0d
Mike <id69702509@yahoo.com.hk>
2017-06-07 23:22:11 +0800

    7/6/2017

--- 32_cards/32.c
+++ 32_cards/32.c
@@ -3,22 +3,17 @@
 #include <stdlib.h>
 struct content
 {
-    char name[20];
-    char value[15][2];
+    char value[15][3];
 };
 
 int main()
 {
-    char value[13][2] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    char value[13][3] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
     struct content club;
     struct content diamonds;
     struct content hearts;
     struct content spades;
 
-    strcpy(club.name, "club");
-    strcpy(diamonds.name, "diamonds");
-    strcpy(hearts.name, "hearts");
-    strcpy(spades.name, "spades");
 
     for (int i = 0; i < 13; i++)
     {
