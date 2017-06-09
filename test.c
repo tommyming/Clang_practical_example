f875bfa7647b22476e691a555130ef1aa39860c6
Mike <id69702509@yahoo.com.hk>
2017-06-10 07:13:54 +0800

    update of debugging config

--- test.c
+++ test.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-int PrintN(int n){
-    if(!n) return 0;
-    PrintN(n-1);
-    printf("%d \n",n);
-    return 0;
-}
+
 int main(int argc, char const *argv[])
 {
-    PrintN(20);
+    printf("hi\n");
     return 0;
 }
